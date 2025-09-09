d9175a0365fe2f44c1250f9c447553d2fdf4a9c5
ksergio <113699828+ksergiocom@users.noreply.github.com>
2025-09-09 17:23:05 +0200

    Simple analytics

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,6 +25,13 @@
         <x-navbar></x-navbar>
     @endunless
     {{ $slot }}
+
+
+
+    <!-- 100% privacy-first analytics -->
+    <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt=""
+            referrerpolicy="no-referrer-when-downgrade" /></noscript>
 </body>
 
 </html>
